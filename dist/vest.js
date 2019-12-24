8b586d147976a7e57faf7c5e3c15b03ee30ac8b0
Travis CI User <@users.noreply.github.com>
2019-12-24 22:42:22 +0000

    ✨ Vest update: 1.0.3
    e4e9112 Update Build configuration. (ealush)

--- dist/vest.js
+++ dist/vest.js
@@ -340,7 +340,7 @@
   /**
    * @type {String} Vest's major version.
    */
-  var VEST_MAJOR = "1.0.2".split('.')[0];
+  var VEST_MAJOR = "1.0.3".split('.')[0];
   /**
    * @type {Symbol} Used to store a global instance of Vest.
    */
@@ -1115,7 +1115,7 @@
   /**
    * @type {String} Version number derived from current tag.
    */
-  var VERSION = "1.0.2";
+  var VERSION = "1.0.3";
 
   var index = singleton.register({
     VERSION: VERSION,

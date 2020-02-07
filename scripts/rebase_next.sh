f8a9e36fa22bfbf0be7364de98dbb471deff4251
ealush <code@ealush.com>
2020-02-08 00:30:47 +0200

    conf: fetch next before merging back

--- scripts/rebase_next.sh
+++ scripts/rebase_next.sh
@@ -2,6 +2,7 @@ git config --global user.email "${EMAIL_ADDRESS}" --replace-all
 git config --global user.name $GIT_NAME
 
 echo "Trying to update next branch"
+git fetch origin next
 git checkout next
 git rebase master || git rebase --abort
 git push https://${GITHUB_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git next

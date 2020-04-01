9dd947a6a2ea3c6446d64cf0aa560cbebb18b3fe
ealush <code@ealush.com>
2020-04-01 22:47:24 +0300

    config: Clean build artifacts before merging back to next

--- scripts/rebase_next.sh
+++ scripts/rebase_next.sh
@@ -3,6 +3,9 @@ git config --global user.name $GIT_NAME
 
 echo "Trying to update next branch"
 git fetch origin next
+
+echo "Cleaning build artifacts"
+git reset --hard
 git checkout next
 git rebase master || git rebase --abort
 git push https://${GITHUB_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git next

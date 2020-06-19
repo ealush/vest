0a5d4b6c5ee5d3c01552f59e6fd6f6ea9cbf7109
ealush <code@ealush.com>
2020-06-19 23:46:32 +0300

    spec: Improve async tests specs

--- util/exec.js
+++ util/exec.js
@@ -1,7 +1,14 @@
 const execSync = require('child_process').execSync;
+const logger = require('./logger');
 
 function exec(command) {
-  return execSync(command, { stdio: 'inherit' });
+  logger.info(`🎬 Executing command: "${command}"`);
+  try {
+    execSync(command, { stdio: 'inherit' });
+  } catch (err) {
+    logger.error(err.message);
+    process.exit(1);
+  }
 }
 
 module.exports = exec;

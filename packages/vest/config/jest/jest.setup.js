1985f285c63ca29414f7e96e1ec1a924d9cb84d3
Andrel Karunia S <andrel.sitanggang@gmail.com>
2020-09-02 21:01:46 +0700

    build: Add __dev__ global to the development build (#366)

--- packages/vest/config/jest/jest.setup.js
+++ packages/vest/config/jest/jest.setup.js
@@ -4,6 +4,7 @@ const { packageJson, packagePath } = require('../../../../util');
 const { version } = packageJson(PACKAGE_VEST);
 global.LIBRARY_NAME = PACKAGE_VEST;
 global.VEST_VERSION = version;
+global.ENV_DEVELOPMENT = true;
 
 // Registers global instance
 require(packagePath(PACKAGE_VEST, 'src'));

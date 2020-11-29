dbb930bd136616e6fdcf0c0a78471103ee6030af
sharonhp_javelin <sharonhp@javelin-networks.com>
2020-11-29 09:47:28 +0200

    Improve 'oneOf' logic

--- packages/n4s/src/enforce/compounds/oneOf.js
+++ packages/n4s/src/enforce/compounds/oneOf.js
@@ -7,8 +7,18 @@ import { withFirst } from 'withArgs';
  * @param {Function[]} rules    Rules to validate the value with
  */
 function oneOf(value, rules) {
-  return one.apply(null,
-    rules.map(ruleGroup => runLazyRules(ruleGroup, value))
-  );
+  let count = 0;
+
+  for (let i = 0; i < rules.length; i++) {
+    if (runLazyRules(rules[i], value)) {
+      count++;
+    }
+
+    if (count > 1) {
+      return false;
+    }
+  }
+
+  return count === 1;
 }
 export default withFirst(oneOf)

8a4b5addecc4e8d274c3bcf81b5ab812aa0d5248
shani.arnon <shani.arnon@doit-intl.com>
2021-02-27 17:43:52 +0200

    add cashe around collectFailureMessages

--- packages/vest/src/core/produce/collectFailureMessages.js
+++ packages/vest/src/core/produce/collectFailureMessages.js
@@ -1,5 +1,10 @@
+import createCache from 'cache';
+import context from 'ctx';
+
 import isMatchingSeverityProfile from 'isMatchingSeverityProfile';
 import useTestObjects from 'useTestObjects';
+
+const cache = createCache();
 
 /**
  * @param {'warn'|'error'} severity   Filter by severity.
@@ -11,35 +16,28 @@ import useTestObjects from 'useTestObjects';
 const collectFailureMessages = (severity, options) => {
   const [testObjects] = useTestObjects();
   const { group, fieldName } = options || {};
-  const res = testObjects.reduce((collector, testObject) => {
-    if (group && testObject.groupName !== group) {
-      return collector;
-    }
+  const { stateRef } = context.use();
+  const ctxRef = { stateRef };
 
-    if (fieldName && testObject.fieldName !== fieldName) {
-      return collector;
-    }
+  return cache(
+    [testObjects, severity, options], context.bind(ctxRef, () => {
+      let res = testObjects.reduce((collector, testObject) => {
+        if (group && testObject.groupName !== group) return collector;
+        if (fieldName && testObject.fieldName !== fieldName) return collector;
+        if (!testObject.failed) return collector;
+        if (isMatchingSeverityProfile(severity, testObject)) return collector;
 
-    if (!testObject.failed) {
-      return collector;
-    }
+        collector[testObject.fieldName] = (
+          collector[testObject.fieldName] || []
+        ).concat(testObject.statement);
 
-    if (isMatchingSeverityProfile(severity, testObject)) {
-      return collector;
-    }
+        return collector;
+      }, {});
 
-    collector[testObject.fieldName] = (
-      collector[testObject.fieldName] || []
-    ).concat(testObject.statement);
-
-    return collector;
-  }, {});
-
-  if (fieldName) {
-    return res[fieldName] || [];
-  } else {
-    return res;
-  }
+      if (fieldName) return res[fieldName] || [];
+      return res;
+    })
+  );
 };
 
 export default collectFailureMessages;

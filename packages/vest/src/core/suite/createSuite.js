ff8b1177f7bc011e80da55e860d5db474d0b54a2
shani.arnon <shani.arnon@doit-intl.com>
2021-02-27 14:09:39 +0200

    linter error fixed

--- packages/vest/src/core/suite/createSuite.js
+++ packages/vest/src/core/suite/createSuite.js
@@ -69,13 +69,10 @@ const createSuite = withArgs(args => {
   });
   suite.hasErrors = (fieldName) => suite.get().hasErrors(fieldName);
   suite.hasWarnings = (fieldName) => suite.get().hasWarnings(fieldName);
-  
   suite.getErrors = (fieldName) => suite.get().getErrors(fieldName);
   suite.getWarnings = (fieldName) => suite.get().getWarnings(fieldName);
-
   suite.hasErrorsByGroup = (groupName, fieldName) => suite.get().hasErrorsByGroup(groupName, fieldName);
   suite.hasWarningsByGroup = (groupName, fieldName) => suite.get().hasWarningsByGroup(groupName, fieldName);
-  
   suite.getErrorsByGroup = (groupName, fieldName) => suite.get().getErrorsByGroup(groupName, fieldName);
   suite.getWarningsByGroup = (groupName, fieldName) => suite.get().getWarningsByGroup(groupName, fieldName);
   return suite;

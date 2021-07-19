c714a9e9dc49683702680885ad12e64fe11a6453
Tomáš Havlas <tomas@havlas.me>
2021-07-19 14:34:30 +0200

    Avoid crashing when Function.name is not configurable

--- packages/__shared/src/setFnName.js
+++ packages/__shared/src/setFnName.js
@@ -1,3 +1,5 @@
 export default function setFnName(fn, value) {
-  return Object.defineProperty(fn, 'name', { value });
+  // Pre ES2015 non standard implementation, "Function.name" is non configurable field
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/name
+  return Object.getOwnPropertyDescriptor(fn, 'name').configurable ? Object.defineProperty(fn, 'name', { value }) : fn;
 }

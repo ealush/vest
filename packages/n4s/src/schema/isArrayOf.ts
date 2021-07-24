f41a444e9dce39c037863965bae716b8210311c4
undefined <code@ealush.com>
2021-07-25 01:18:31 +0300

    feat(n4s): context propagation within enforce

--- packages/n4s/src/schema/isArrayOf.ts
+++ packages/n4s/src/schema/isArrayOf.ts
@@ -1,16 +1,20 @@
 import mapFirst from 'mapFirst';
 
+import { ctx } from 'enforceContext';
 import type { TRuleDetailedResult, TLazyRuleMethods } from 'ruleReturn';
 import * as ruleReturn from 'ruleReturn';
 import runLazyRule from 'runLazyRule';
 
 export default function isArrayOf(
   inputArray: any[],
-  ruleChain: TLazyRuleMethods
+  currentRule: TLazyRuleMethods
 ): TRuleDetailedResult {
   return (
-    mapFirst(inputArray, (item, breakout) => {
-      const res = runLazyRule(ruleChain, item);
+    mapFirst(inputArray, (currentValue, breakout, index) => {
+      const res = ctx.run(
+        { value: currentValue, set: true, meta: { index } },
+        () => runLazyRule(currentRule, currentValue)
+      );
 
       if (!res.pass) {
         breakout(res);

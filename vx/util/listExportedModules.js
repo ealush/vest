22fe53a399335950b95cdb4f475c26e7b8f722f3
undefined <code@ealush.com>
2021-07-27 20:29:28 +0300

    config: add more extensive exports mapping

--- vx/util/listExportedModules.js
+++ vx/util/listExportedModules.js
@@ -7,9 +7,11 @@ const packageName = require('vx/packageName');
 const vxPath = require('vx/vxPath');
 
 function listExportedModules(pkgName = packageName()) {
-  return glob
-    .sync(vxPath.packageSrc(pkgName, opts.dir.EXPORTS, '*.ts'))
-    .map(f => path.basename(f, '.ts'));
+  return (
+    glob
+      .sync(vxPath.packageSrc(pkgName, opts.dir.EXPORTS, '*.ts'))
+      .map(f => path.basename(f, '.ts')) ?? []
+  );
 }
 
 module.exports = listExportedModules;

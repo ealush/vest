f3d11c6614147c2d42144d02af75b97d3bf914bd
Václav Jančařík <vaclav.janc@gmail.com>
2021-07-30 14:44:43 +0200

    Fix crashing of setFnName in IE11

--- packages/__shared/src/setFnName.js
+++ packages/__shared/src/setFnName.js
@@ -1,5 +1,7 @@
 export default function setFnName(fn, value) {
   // Pre ES2015 non standard implementation, "Function.name" is non configurable field
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/name
-  return Object.getOwnPropertyDescriptor(fn, 'name').configurable ? Object.defineProperty(fn, 'name', { value }) : fn;
+  return Object.getOwnPropertyDescriptor(fn, 'name')?.configurable
+    ? Object.defineProperty(fn, 'name', { value })
+    : fn;
 }

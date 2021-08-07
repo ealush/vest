3daeb68034ad95a0684940bfb068d90f3cc73f8b
ealush <code@ealush.com>
2021-08-07 20:31:53 +0300

    types(n4s): simplify the types for

--- packages/n4s/src/modifiers/partial.ts
+++ packages/n4s/src/modifiers/partial.ts
@@ -1,10 +1,14 @@
 import enforce from 'enforce';
-import type { TShapeObject, TLazy } from 'genEnforceLazy';
 
-export default function partial(shapeObject: TShapeObject): TShapeObject {
-  const output: TShapeObject = {};
+// Help needed improving the typings of this file.
+// Ideally, we'd be able to extend TShapeObject, but that's not possible.
+export default function partial<T extends Record<any, any>>(shapeObject: T): T {
+  const output = {} as T;
   for (const key in shapeObject) {
-    output[key] = enforce.optional(shapeObject[key]) as TLazy;
+    output[key] = enforce.optional(shapeObject[key]) as T[Extract<
+      keyof T,
+      string
+    >];
   }
 
   return output;

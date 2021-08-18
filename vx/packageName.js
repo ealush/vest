55810a225633324ac85f3dc85662c9b2f644e428
ealush <code@ealush.com>
2021-08-19 00:59:42 +0300

    conf: remove doc generation from vx

--- vx/packageName.js
+++ vx/packageName.js
@@ -1,5 +1,14 @@
+const path = require('path');
+
 const ctx = require('vx/vxContext');
+const vxPath = require('vx/vxPath');
 
 module.exports = function packageName() {
   return ctx.usePackage() ?? process.env.VX_PACKAGE_NAME; // VX_PACKAGE_NAME is only used by rollup (buildPackage.js)
+};
+
+module.exports.main = function () {
+  const packageJson = require(path.resolve(vxPath.ROOT_PATH, 'package.json'));
+
+  return packageJson.vx.main;
 };

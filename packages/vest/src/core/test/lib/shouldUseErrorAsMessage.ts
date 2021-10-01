4cb2c6c4c4e22fc9abd0a86f13aed42abb4de49e
ealush <code@ealush.com>
2021-10-02 02:14:59 +0300

    types(vest): add safeguard around shouldUseErrorAsMessage

--- packages/vest/src/core/test/lib/shouldUseErrorAsMessage.ts
+++ packages/vest/src/core/test/lib/shouldUseErrorAsMessage.ts
@@ -3,7 +3,8 @@ import { isUndefined } from 'isUndefined';
 
 export default function shouldUseErrorAsMessage(
   message: string | void,
-  error: any
-): boolean {
+  error: unknown
+): error is string {
+  // kind of cheating with this safe guard, but it does the job
   return isUndefined(message) && isStringValue(error);
 }

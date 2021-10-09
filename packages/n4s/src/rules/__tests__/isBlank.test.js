0dd1ec1103a544b8207b48c1be8d227fa0a515df
Pizzo15 <lpizzini7@gmail.com>
2021-10-09 15:33:19 +0200

    chore: added isBlank rule

---
+++ packages/n4s/src/rules/__tests__/isBlank.test.js
@@ -0,0 +1,21 @@
+import { isBlank, isNotBlank } from 'isBlank';
+
+describe('isBlank', () => {
+  it('Should return true for a string of whitespaces', () => {
+    expect(isBlank('   ')).toBe(true);
+  });
+
+  it('Should return false for a string with at least a non-whitespace', () => {
+    expect(isBlank('not blank')).toBe(false);
+  });
+});
+
+describe('isNotBlank', () => {
+  it('Should return false for a string of whitespaces', () => {
+    expect(isNotBlank('   ')).toBe(false);
+  });
+
+  it('Should return true for a string with at least a non-whitespace', () => {
+    expect(isNotBlank('not blank')).toBe(true);
+  });
+});

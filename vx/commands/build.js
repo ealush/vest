2dfedaf20438bbb415e6096dda15b141a28ced6f
ealush <code@ealush.com>
2021-10-16 01:09:28 +0300

    vx: correctly pass cli options to build script

--- vx/commands/build.js
+++ vx/commands/build.js
@@ -3,7 +3,7 @@ const buildPackage = require('../scripts/build/buildPackage');
 const runOnActivePackages = require('vx/util/runOnActivePackages');
 
 function build({ options }) {
-  runOnActivePackages(buildPackage, options);
+  runOnActivePackages(buildPackage, { options });
 }
 
 module.exports = build;

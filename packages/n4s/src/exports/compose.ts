7473483ff8fcb960baf404e713a5d7573f6d122f
ealush <code@ealush.com>
2021-11-03 23:51:46 +0200

    hack(n4s): use 'any' as the input for compose

--- packages/n4s/src/exports/compose.ts
+++ packages/n4s/src/exports/compose.ts
@@ -7,8 +7,11 @@ import { isEmpty } from 'isEmpty';
 import { defaultToPassing, TRuleDetailedResult } from 'ruleReturn';
 import runLazyRule from 'runLazyRule';
 
-// eslint-disable-next-line max-lines-per-function
-export default function compose(...composites: TLazy[]): ((
+/* eslint-disable max-lines-per-function */
+
+// TODO: This gives me a headache. Instead of `any` we should use `TLazy`
+// but it fails when using compose. The type is very complex.
+export default function compose(...composites: any[]): ((
   value: any
 ) => void) & {
   run: (value: any) => TRuleDetailedResult;

81bdcd24b2a56884fcb3ed99b66ec355d787a098
ealush <code@ealush.com>
2021-11-30 23:59:08 +0200

    Add initial key support - doesn't do anything yet

--- packages/vest/src/core/test/test.ts
+++ packages/vest/src/core/test/test.ts
@@ -1,4 +1,7 @@
 import assign from 'assign';
+import isFunction from 'isFunction';
+import { isNotString } from 'isString';
+import throwError from 'throwError';
 
 import VestTest, { TTestFn } from 'VestTest';
 import ctx from 'ctx';
@@ -6,20 +9,44 @@ import registerPrevRunTest from 'registerPrevRunTest';
 import bindTestEach from 'test.each';
 import bindTestMemo from 'test.memo';
 
+function testBase(fieldName: string, message: string, cb: TTestFn): VestTest;
+function testBase(fieldName: string, cb: TTestFn): VestTest;
 function testBase(
   fieldName: string,
-  ...args: [message: string, cb: TTestFn]
+  message: string,
+  cb: TTestFn,
+  key: string
 ): VestTest;
-function testBase(fieldName: string, ...args: [cb: TTestFn]): VestTest;
+function testBase(fieldName: string, cb: TTestFn, key: string): VestTest;
 function testBase(
   fieldName: string,
-  ...args: [message: string, cb: TTestFn] | [cb: TTestFn]
+  ...args:
+    | [message: string, cb: TTestFn]
+    | [cb: TTestFn]
+    | [message: string, cb: TTestFn, key: string]
+    | [cb: TTestFn, key: string]
 ): VestTest {
-  const [testFn, message] = args.reverse() as [TTestFn, string | undefined];
+  const [message, testFn, key] = isFunction(args[1])
+    ? args
+    : ([undefined, ...args] as [
+        string | undefined,
+        TTestFn,
+        string | undefined
+      ]);
+
+  if (isNotString(fieldName)) {
+    throwIncompatibleParamsError('fieldName', 'string');
+  }
+
+  if (!isFunction(testFn)) {
+    throwIncompatibleParamsError('Test callback', 'function');
+  }
+
   const context = ctx.useX();
   const testObject = new VestTest(fieldName, testFn, {
     message,
     groupName: context.groupName,
+    key,
   });
 
   return registerPrevRunTest(testObject);
@@ -31,3 +58,9 @@ export default assign(testBase, {
 });
 
 export type TTestBase = typeof testBase;
+
+function throwIncompatibleParamsError(name: string, expected: string) {
+  throwError(
+    `Incompatible params passed to test function. ${name} must be a ${expected}`
+  );
+}

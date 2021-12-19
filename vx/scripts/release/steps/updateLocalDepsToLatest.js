eb4e35e43747e89c18bdc1259763377f06befd2a
ealush <code@ealush.com>
2021-12-20 01:44:50 +0200

    vx: use caret version when bumping local deps

--- vx/scripts/release/steps/updateLocalDepsToLatest.js
+++ vx/scripts/release/steps/updateLocalDepsToLatest.js
@@ -1,8 +1,15 @@
 const fse = require('fs-extra');
 
+const { TAG_NEXT, TAG_DEV } = require('../releaseKeywords');
+
 const logger = require('vx/logger');
 const packageNames = require('vx/packageNames');
 const packageJson = require('vx/util/packageJson');
+const {
+  isNextBranch,
+  isIntegrationBranch,
+  targetPackage,
+} = require('vx/util/taggedBranch');
 const vxPath = require('vx/vxPath');
 
 // eslint-disable-next-line complexity
@@ -23,7 +30,21 @@ module.exports = function updateLocalDepsToLatest() {
     const depPkgJson = packageJson(name);
 
     if (depPkgJson && depPkgJson.name === name) {
-      deps[name] = depPkgJson.version;
+      const latestKnown = `^${depPkgJson.version}`;
+
+      // If we have a "targetPackage", we might not be building our dependencies, so start with
+      // taking the latest from the current tag. Might not be perfect, but since this is mostly
+      // used for development or "next", it's not too bad, and we're defaulting to the latest
+      // regardless.
+      if (targetPackage) {
+        deps[name] = isNextBranch
+          ? TAG_NEXT
+          : isIntegrationBranch
+          ? TAG_DEV
+          : latestKnown;
+      } else {
+        deps[name] = latestKnown;
+      }
     }
   }
 

04c8340f4a44dc82e99563ac5faedcc579dbe464
galezra <galezra@gmail.com>
2022-01-11 23:35:27 +0200

    fix: improving tests and updating docs

--- packages/n4s/src/rules/__tests__/isKeyOf.test.ts
+++ packages/n4s/src/rules/__tests__/isKeyOf.test.ts
@@ -1,4 +1,5 @@
-import { isKeyOf } from 'isKeyOf';
+import { enforce } from 'enforce';
+import { isKeyOf, isNotKeyOf } from 'isKeyOf';
 
 const FRUITES = {
   apples: 5,
@@ -15,22 +16,47 @@ const TOP_GROSSING_MOVIES = {
 const DUMMY_KEY = 'key';
 
 describe('Tests isKeyOf rule', () => {
-  it('When the key exists in the object', () => {
-    expect(isKeyOf('bananas', FRUITES)).toBe(true);
+  describe('When the key exists in the object', () => {
+    it('Should return true', () => {
+      expect(isKeyOf('bananas', FRUITES)).toBe(true);
+      expect(isKeyOf(1976, TOP_GROSSING_MOVIES)).toBe(true);
+    });
+
+    it('Should return true using enforce', () => {
+      enforce('bananas').isKeyOf(FRUITES);
+      enforce(1976).isKeyOf(TOP_GROSSING_MOVIES);
+    });
   });
 
-  it('When the key not exists in the object', () => {
-    expect(isKeyOf('avocados', FRUITES)).toBe(false);
+  describe('When the key does not exists in the object', () => {
+    it('Should return false', () => {
+      expect(isKeyOf('avocado', FRUITES)).toBe(false);
+      expect(isKeyOf(1999, TOP_GROSSING_MOVIES)).toBe(false);
+    });
+
+    it.each([undefined, null, false, true, Object, [], '', Function.prototype])(
+      'Should throw when %s is an object',
+      v => {
+        expect(() => enforce(DUMMY_KEY).isKeyOf({ v })).toThrow();
+      }
+    );
+  });
+});
+
+describe('Tests isNotKeyOf rule', () => {
+  describe('When the key does not exists in the object', () => {
+    it('Should return true', () => {
+      expect(isNotKeyOf('avocado', FRUITES)).toBe(true);
+    });
+
+    it('Should return true using enforce', () => {
+      enforce(1999).isNotKeyOf(TOP_GROSSING_MOVIES);
+    });
   });
 
-  it('When the key exists in the object (numeric)', () => {
-    expect(isKeyOf(1976, TOP_GROSSING_MOVIES)).toBe(true);
+  describe('When the key exists in the object', () => {
+    it('Should return false', () => {
+      expect(isNotKeyOf(1976, TOP_GROSSING_MOVIES)).toBe(false);
+    });
   });
-
-  it.each([undefined, null, false, true, Object, [], '', Function.prototype])(
-    'Should return false for %s object',
-    v => {
-      expect(isKeyOf(DUMMY_KEY, v)).toBe(false);
-    }
-  );
 });
--- packages/n4s/src/rules/isKeyOf.ts
+++ packages/n4s/src/rules/isKeyOf.ts
@@ -1,13 +1,7 @@
 import bindNot from 'bindNot';
-import { isBoolean } from 'isBooleanValue';
-import { isNullish } from 'isNullish';
-import { isStringValue } from 'isStringValue';
 
 export function isKeyOf(key: string | symbol | number, obj: any): boolean {
-  const shouldTerminate =
-    isNullish(obj) || isBoolean(obj) || isStringValue(obj);
-
-  return shouldTerminate ? false : key in obj;
+  return key in obj;
 }
 
 export const isNotKeyOf = bindNot(isKeyOf);

3f9811e15b2b599c16e1600409adf79a90f086a8
ealush <code@ealush.com>
2022-03-25 15:25:39 +0300

    test(vest): make collectFailureMessages tests use external interface

--- packages/vest/src/core/suite/produce/getFailures/__tests__/collectFailureMessages.test.ts
+++ packages/vest/src/core/suite/produce/getFailures/__tests__/collectFailureMessages.test.ts
@@ -1,21 +1,19 @@
+// TODO: Verify this test file is not needed and delete it
+import { dummyTest } from '../../../../../../testUtils/testDummy';
+
 import { Severity } from 'Severity';
 import VestTest from 'VestTest';
 import collectFailureMessages from 'collectFailureMessages';
+import create from 'create';
+import group from 'group';
+import { SuiteResult } from 'produceSuiteResult';
+import * as vest from 'vest';
 
 describe('collectFailureMessages', () => {
-  let testObjects: VestTest[] = [];
-
-  it('Should return an object containing just the requested field', () => {
-    const result = collectFailureMessages(Severity.ERRORS, testObjects, {
-      fieldName: 'field_1',
-    });
-    expect(Object.keys(result)).toEqual(['field_1']);
-  });
+  let suite, res: SuiteResult;
 
   test('Result has an array of matching error messages', () => {
-    const result = collectFailureMessages(Severity.ERRORS, testObjects, {
-      fieldName: 'field_1',
-    });
+    const result = res.getErrors();
     expect(result.field_1).toEqual([
       'field_1_failure message 2',
       'field_1_failure message 3',
@@ -23,19 +21,16 @@ describe('collectFailureMessages', () => {
   });
 
   it('Should return filtered messages by the selected group', () => {
-    const result = collectFailureMessages(Severity.ERRORS, testObjects, {
-      group: 'group1',
-    });
+    const result = res.getErrorsByGroup('group_1');
 
-    expect(result).toEqual({ field_2: ['field_2_failure message 3'] });
+    expect(result).toEqual({
+      field_1: [],
+      field_2: ['field_2_failure message 1', 'field_2_failure message 3'],
+    });
   });
 
   it('Should return an empty object when no options and no failures', () => {
-    expect(
-      collectFailureMessages(Severity.ERRORS, [
-        new VestTest('field_1', jest.fn(), { message: 'error_message' }),
-      ])
-    ).toEqual({});
+    expect(res.getErrors('nonexistent_field')).toEqual([]);
   });
 
   it('Should return an object with an empty array when selected field has no errors', () => {
@@ -48,126 +43,85 @@ describe('collectFailureMessages', () => {
     ).toEqual({ field_1: [] });
   });
 
-  [Severity.ERRORS, Severity.WARNINGS].forEach((severity: Severity) => {
-    describe('Snapshot tests. severity: ' + severity, () => {
-      describe('When no options passed', () => {
-        it('should match snapshot', () => {
-          expect(
-            collectFailureMessages(severity, testObjects)
-          ).toMatchSnapshot();
-        });
+  describe('getErrors', () => {
+    describe('When no options passed', () => {
+      it('should match snapshot', () => {
+        expect(res.getErrors()).toMatchSnapshot();
       });
+    });
 
-      describe('When specific field requested', () => {
-        it('Should match snapshot', () => {
-          expect(
-            collectFailureMessages(severity, testObjects, {
-              fieldName: 'field_1',
-            })
-          ).toMatchSnapshot();
-          expect(
-            collectFailureMessages(severity, testObjects, {
-              fieldName: 'field_2',
-            })
-          ).toMatchSnapshot();
-          expect(
-            collectFailureMessages(severity, testObjects, {
-              fieldName: 'field_3',
-            })
-          ).toMatchSnapshot();
-        });
+    describe('When specific field requested', () => {
+      it('Should match snapshot', () => {
+        expect(res.getErrors('field_1')).toMatchSnapshot();
+        expect(res.getErrors('field_2')).toMatchSnapshot();
+        expect(res.getErrors('field_3')).toMatchSnapshot();
       });
+    });
+  });
 
-      describe('When specific group requested', () => {
-        it('Should match snapshot', () => {
-          expect(
-            collectFailureMessages(severity, testObjects, {
-              group: 'group1',
-            })
-          ).toMatchSnapshot();
-          expect(
-            collectFailureMessages(severity, testObjects, {
-              group: 'group1',
-            })
-          ).toMatchSnapshot();
-          expect(
-            collectFailureMessages(severity, testObjects, {
-              group: 'group1',
-            })
-          ).toMatchSnapshot();
-        });
+  describe('getWarnings', () => {
+    describe('When no options passed', () => {
+      it('should match snapshot', () => {
+        expect(res.getWarnings()).toMatchSnapshot();
       });
+    });
 
-      describe('When a group-fieldName combo is requested', () => {
-        it('Should match snapshot', () => {
-          expect(
-            collectFailureMessages(severity, testObjects, {
-              group: 'group1',
-              fieldName: 'field_2',
-            })
-          ).toMatchSnapshot();
-        });
+    describe('When specific field requested', () => {
+      it('Should match snapshot', () => {
+        expect(res.getWarnings('field_1')).toMatchSnapshot();
+        expect(res.getWarnings('field_2')).toMatchSnapshot();
+        expect(res.getWarnings('field_3')).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe('getErrorsByGroup', () => {
+    it('Should match snapshot', () => {
+      expect(res.getErrorsByGroup('group_1')).toMatchSnapshot();
+    });
+
+    describe('with field name', () => {
+      it('Should match snapshot', () => {
+        expect(res.getErrorsByGroup('group_1', 'field_1')).toMatchSnapshot();
+        expect(res.getErrorsByGroup('group_1', 'field_2')).toMatchSnapshot();
+        expect(res.getErrorsByGroup('group_1', 'field_3')).toMatchSnapshot();
+      });
+    });
+  });
+  describe('getWarningsByGroup', () => {
+    it('Should match snapshot', () => {
+      expect(res.getWarningsByGroup('group_1')).toMatchSnapshot();
+    });
+
+    describe('with field name', () => {
+      it('Should match snapshot', () => {
+        expect(res.getWarningsByGroup('group_1', 'field_1')).toMatchSnapshot();
+        expect(res.getWarningsByGroup('group_1', 'field_2')).toMatchSnapshot();
+        expect(res.getWarningsByGroup('group_1', 'field_3')).toMatchSnapshot();
       });
     });
   });
 
   beforeEach(() => {
-    testObjects = [
-      new VestTest('field_1', jest.fn(), {
-        message: 'field_1_failure message 1',
-      }),
-      (() => {
-        const test = new VestTest('field_1', jest.fn(), {
-          message: 'field_1_failure message 2',
-        });
-        test.fail();
-        return test;
-      })(),
-      (() => {
-        const test = new VestTest('field_1', jest.fn(), {
-          message: 'field_1_failure message 3',
-        });
-        test.fail();
-        return test;
-      })(),
-      new VestTest('field_1', jest.fn(), { groupName: 'group1' }),
-      new VestTest('field_2', jest.fn(), {
-        groupName: 'group1',
-        message: 'field_2_failure message 1',
-      }),
-      (() => {
-        const test = new VestTest('field_2', jest.fn(), {
-          message: 'field_2_warning message 1',
-        });
-        test.warn();
-        return test;
-      })(),
-      (() => {
-        const test = new VestTest('field_2', jest.fn(), {
-          message: 'field_2_warning message 2',
-        });
-        test.fail();
-        test.warn();
-        return test;
-      })(),
-      (() => {
-        const test = new VestTest('field_2', jest.fn(), {
-          groupName: 'group1',
-          message: 'field_2_warning message 3',
-        });
-        test.fail();
-        test.warn();
-        return test;
-      })(),
-      (() => {
-        const test = new VestTest('field_2', jest.fn(), {
-          groupName: 'group1',
-          message: 'field_2_failure message 3',
-        });
-        test.fail();
-        return test;
-      })(),
-      new VestTest('field_3', jest.fn()),
-    ];
+    suite = create(() => {
+      dummyTest.passing('field_1', 'field_1_failure message 1');
+      dummyTest.failing('field_1', 'field_1_failure message 2');
+      dummyTest.failing('field_1', 'field_1_failure message 3');
+      group('group_1', () => {
+        vest.test('field_1', () => false);
+        vest.test('field_3', () => {});
+      });
+      dummyTest.failing('field_2', 'field_2_failure message 1', 'group_1');
+      dummyTest.passingWarning('field_2', 'field_2_warning message 1');
+      dummyTest.failingWarning('field_2', 'field_2_warning message 2');
+      dummyTest.failingWarning(
+        'field_2',
+        'field_2_warning message 3',
+        'group_1'
+      );
+      dummyTest.failing('field_2', 'field_2_failure message 3', 'group_1');
+    });
+    suite();
+    res = suite.get();
   });
 });

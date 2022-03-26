a8484a839063e1f7879e8be429f5f651bee5ecee
ealush <code@ealush.com>
2022-03-26 21:28:01 +0100

    patch(vest): make suite result methods use static summary object

--- packages/vest/src/core/suite/produce/produceSuiteResult.ts
+++ packages/vest/src/core/suite/produce/produceSuiteResult.ts
@@ -22,7 +22,7 @@ export function produceSuiteResult(): SuiteResult {
     ctx.bind(ctxRef, () => {
       const summary = genTestsSummary();
       const suiteName = useSuiteName();
-      const ref = { summary, ...ctxRef };
+      const ref = assign({ summary }, ctxRef);
       return assign(summary, {
         getErrors: ctx.bind(ref, getErrors),
         getErrorsByGroup: ctx.bind(ref, getErrorsByGroup),

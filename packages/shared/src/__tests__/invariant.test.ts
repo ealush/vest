852c69225de573d4898004ef687d0daec5a50f6b
ealush <code@ealush.com>
2022-04-01 01:22:54 +0300

    tests: invariant

---
+++ packages/shared/src/__tests__/invariant.test.ts
@@ -0,0 +1,45 @@
+import invariant from 'invariant';
+
+describe('invariant', () => {
+  it('should throw an error when condition is false', () => {
+    expect(() => {
+      invariant(false, 'message');
+    }).toThrow(Error);
+  });
+
+  it("Should throw an error with the message if it's a string", () => {
+    expect(() => {
+      invariant(false, 'message');
+    }).toThrow('message');
+  });
+
+  it('should contintue when condition is true', () => {
+    expect(() => {
+      invariant(true, 'message');
+    }).not.toThrow();
+  });
+
+  describe('When passed message is a string object', () => {
+    it('should throw the value of the string object', () => {
+      expect(() => {
+        invariant(false, new String('message'));
+      }).toThrow('message');
+    });
+  });
+
+  describe('Shen passed message is a function', () => {
+    it('should throw the value of the function', () => {
+      expect(() => {
+        invariant(false, () => 'message');
+      }).toThrow('message');
+    });
+  });
+
+  describe('When message is falsy', () => {
+    it('should throw an error with the message', () => {
+      expect(() => {
+        invariant(false, '');
+      }).toThrow('');
+    });
+  });
+});

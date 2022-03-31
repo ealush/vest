584be8a7fc14471c97fa3090e20a4f4b5dbd729b
ealush <code@ealush.com>
2022-04-01 01:52:51 +0300

    patch(vest): remove duplicate VestTest run logic

--- packages/vest/src/core/test/runSyncTest.ts
+++ packages/vest/src/core/test/runSyncTest.ts
@@ -1,25 +1,8 @@
 import VestTest, { TestResult } from 'VestTest';
 import context from 'ctx';
-import shouldUseErrorAsMessage from 'shouldUseErrorAsMessage';
 /**
  * Runs sync tests - or extracts promise.
  */
 export default function runSyncTest(testObject: VestTest): TestResult {
-  return context.run({ currentTest: testObject }, () => {
-    let result;
-    try {
-      result = testObject.testFn();
-    } catch (e) {
-      if (shouldUseErrorAsMessage(testObject.message, e)) {
-        testObject.message = e;
-      }
-      result = false;
-    }
-
-    if (result === false) {
-      testObject.fail();
-    }
-
-    return result;
-  });
+  return context.run({ currentTest: testObject }, () => testObject.run());
 }

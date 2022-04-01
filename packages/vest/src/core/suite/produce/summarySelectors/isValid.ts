b20ef64d78678ad24ed4966298a2b0882e28bf4d
ealush <code@ealush.com>
2022-04-01 15:12:55 +0300

    tests(vest): isValid

--- packages/vest/src/core/suite/produce/summarySelectors/isValid.ts
+++ packages/vest/src/core/suite/produce/summarySelectors/isValid.ts
@@ -3,5 +3,5 @@ import { useSummary } from 'genTestsSummary';
 export function isValid(fieldName?: string): boolean {
   const summary = useSummary();
 
-  return Boolean(fieldName ? summary.tests?.[fieldName]?.valid : summary.valid);
+  return fieldName ? Boolean(summary.tests[fieldName]?.valid) : summary.valid;
 }

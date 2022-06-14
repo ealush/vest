555bf856910a941035694940deca600f2042fe1c
ealush <code@ealush.com>
2022-06-15 01:16:44 +0300

    patch(n4s): replace array with counter in oneOf

--- packages/n4s/src/plugins/compounds/oneOf.ts
+++ packages/n4s/src/plugins/compounds/oneOf.ts
@@ -1,24 +1,24 @@
+import { equals } from 'equals';
 import type { Lazy } from 'genEnforceLazy';
-import { lengthEquals } from 'lengthEquals';
-import { longerThan } from 'longerThan';
+import { greaterThan } from 'greaterThan';
 import ruleReturn, { RuleDetailedResult } from 'ruleReturn';
 import runLazyRule from 'runLazyRule';
 
 const REQUIRED_COUNT = 1;
 
 export function oneOf(value: unknown, ...rules: Lazy[]): RuleDetailedResult {
-  const passing: RuleDetailedResult[] = [];
+  let passingCount = 0;
   rules.some(rule => {
     const res = runLazyRule(rule, value);
 
     if (res.pass) {
-      passing.push(res);
+      passingCount++;
     }
 
-    if (longerThan(passing, REQUIRED_COUNT)) {
+    if (greaterThan(passingCount, REQUIRED_COUNT)) {
       return false;
     }
   });
 
-  return ruleReturn(lengthEquals(passing, REQUIRED_COUNT));
+  return ruleReturn(equals(passingCount, REQUIRED_COUNT));
 }

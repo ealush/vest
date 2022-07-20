5b93c8c19cbfed3f3f973b80e1e312aa8c7ea3b2
Evyatar <code@ealush.com>
2022-07-21 02:14:13 +0300

    vx: run all formats at once (#909)

--- vx/config/rollup/format.js
+++ vx/config/rollup/format.js
@@ -2,7 +2,14 @@ const yargs = require('yargs/yargs');
 
 const opts = require('vx/opts');
 
-const format = yargs(process.argv).argv.format;
+const format = [].concat(
+  yargs(process.argv).argv.format ?? [
+    opts.format.CJS,
+    opts.format.ES,
+    opts.format.UMD,
+  ]
+);
+
 const disallowExternals = format === opts.format.UMD;
 
 module.exports = {

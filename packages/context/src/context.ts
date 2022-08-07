00e679f3a3a52e129040c2dfb184053614a59686
ealush <code@ealush.com>
2022-08-08 01:03:39 +0300

    cascadeContext uses context

--- packages/context/src/context.ts
+++ packages/context/src/context.ts
@@ -34,61 +34,48 @@ export function createContext<T extends unknown>(
 
 // eslint-disable-next-line max-lines-per-function
 export function createCascade<T extends Record<string, unknown>>(
-  init?: (ctxRef: Partial<T>, parentContext: T | void) => T | null
+  init?: (value: Partial<T>, parentContext: T | void) => T | null
 ): CtxCascadeReturn<T> {
-  let contextValue = undefined;
+  const ctx = createContext<T>();
 
   return {
     bind,
     run,
-    use,
+    use: ctx.use,
     useX,
   };
 
   function useX(errorMessage?: string): T {
-    const ctx = use();
+    const contextValue = ctx.use();
     invariant(
-      ctx,
+      contextValue,
       defaultTo(errorMessage, 'Context was used after it was closed')
     );
-    return ctx;
+    return contextValue;
   }
 
-  function run<R>(ctxRef: Partial<T>, fn: () => R): R {
-    const parentContext = use();
+  function run<R>(value: Partial<T>, fn: () => R): R {
+    const parentContext = ctx.use();
 
     const out = assign(
       {},
       parentContext ? parentContext : {},
-      optionalFunctionValue(init, ctxRef, parentContext) ?? ctxRef
+      optionalFunctionValue(init, value, parentContext) ?? value
     ) as T;
 
-    set(Object.freeze(out));
-
-    const res = fn();
-
-    contextValue = parentContext;
-    return res;
+    return ctx.run(Object.freeze(out), fn);
   }
 
-  function bind<Fn extends CB>(ctxRef: Partial<T>, fn: Fn) {
+  function bind<Fn extends CB>(value: Partial<T>, fn: Fn) {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore - this one's pretty hard to get right
     const returnedFn: Fn = function (...runTimeArgs: Parameters<Fn>) {
-      return run<ReturnType<Fn>>(ctxRef, function () {
+      return run<ReturnType<Fn>>(value, function () {
         return fn(...runTimeArgs);
       });
     };
 
     return returnedFn;
-  }
-
-  function use() {
-    return contextValue;
-  }
-
-  function set(value: T): T {
-    return (contextValue = value);
   }
 }
 
@@ -98,8 +85,8 @@ type CtxReturn<T> = {
 };
 
 export type CtxCascadeReturn<T> = {
-  run: <R>(ctxRef: Partial<T>, fn: () => R) => R;
-  bind: <Fn extends CB>(ctxRef: Partial<T>, fn: Fn) => Fn;
+  run: <R>(value: Partial<T>, fn: () => R) => R;
+  bind: <Fn extends CB>(value: Partial<T>, fn: Fn) => Fn;
   use: () => T | undefined;
   useX: (errorMessage?: string) => T;
 };

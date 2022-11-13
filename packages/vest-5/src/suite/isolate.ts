fb743b69fb4ffb2b94964e5ef97bfa36e7b30892
ealush <code@ealush.com>
2022-11-14 01:13:40 +0200

    Create isolate

--- packages/vest-5/src/suite/isolate.ts
+++ packages/vest-5/src/suite/isolate.ts
@@ -1,17 +1,9 @@
+import { IsolateTypes } from './isolateTypes';
+
 function isolate<T>(type: IsolateTypes, callback: IsolateCb<T>): T {
   const result = callback();
 
   return result;
 }
-
-export enum IsolateTypes {
-  DEFAULT,
-}
-
-export type Isolate = {
-  type: IsolateTypes;
-  cursor: number;
-  children: Isolate[];
-};
 
 type IsolateCb<T> = (...args: any[]) => T;

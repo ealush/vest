9de7155e3333b4e5e12860615c31190d2a145487
ealush <code@ealush.com>
2022-11-14 01:33:32 +0200

    Base Isolate Implementation

--- packages/vest-5/src/suite/isolate.ts
+++
@@ -1,9 +0,0 @@
-import { IsolateTypes } from './isolateTypes';
-
-function isolate<T>(type: IsolateTypes, callback: IsolateCb<T>): T {
-  const result = callback();
-
-  return result;
-}
-
-type IsolateCb<T> = (...args: any[]) => T;
--- packages/vest-5/src/suite/isolateTypes.ts
+++
@@ -1,9 +0,0 @@
-export enum IsolateTypes {
-  DEFAULT,
-}
-
-export type Isolate = {
-  type: IsolateTypes;
-  cursor: number;
-  children: Isolate[];
-};

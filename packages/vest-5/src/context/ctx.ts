1c8d9d540959acb37f7cfc324c2902e32c0ced63
ealush <code@ealush.com>
2022-11-16 00:35:11 +0200

    Run group |

--- packages/vest-5/src/context/ctx.ts
+++ packages/vest-5/src/context/ctx.ts
@@ -5,6 +5,7 @@ import { Isolate } from 'isolateTypes';
 
 export const suiteRuntime = createContext<SuiteRuntime>();
 export const currentTest = createContext<VestTest | undefined>(undefined);
+export const currentGroup = createContext<string | undefined>(undefined);
 
 type SuiteRuntime = Isolate<unknown>;
 
@@ -18,4 +19,8 @@ export function useIsolate() {
 
 export function useCurrentTest() {
   return currentTest.use();
+}
+
+export function useCurrentGroup() {
+  return currentGroup.use();
 }

ce1d7ac14bbbe6cd9ec7e02f089c8d662ae01d58
ealush <code@ealush.com>
2022-11-16 00:48:52 +0200

    clear errors

--- packages/vest-5/src/suite/createSuite.ts
+++ packages/vest-5/src/suite/createSuite.ts
@@ -11,17 +11,23 @@ function createSuite<T extends CB>(suiteCallback: T): Suite<T>;
 function createSuite<T extends CB>(
   ...args: [suiteName: SuiteName, suiteCallback: T] | [suiteCallback: T]
 ): Suite<T> {
-  const [suiteCallback, suiteName] = args.reverse() as [T, SuiteName];
+  const [suiteCallback /*suiteName*/] = args.reverse() as [T, SuiteName];
 
-  isolate(IsolateTypes.SUITE, () => {
-    suiteCallback();
-  });
+  function suite() {
+    isolate(IsolateTypes.SUITE, () => {
+      suiteCallback();
+    });
 
-  return {};
+    return {};
+  }
+
+  return suite;
 }
 
 export type SuiteName = string | void;
 
-export type Suite<T extends CB> = Record<string, any>;
+export type Suite<T extends CB> = (...args: any[]) => SuiteMethods<T>;
+
+type SuiteMethods<T> = Record<string, T>;
 
 export { createSuite };

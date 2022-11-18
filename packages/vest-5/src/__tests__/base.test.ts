e25cb76bf5654560f6f2aa358b5ec22685c9507a
ealush <code@ealush.com>
2022-11-19 01:34:38 +0200

    vest@5: Add isolate walker

---
+++ packages/vest-5/src/__tests__/base.test.ts
@@ -0,0 +1,15 @@
+import { createSuite, test, group } from 'vest-5';
+
+describe('base scenario', () => {
+  it('Should reflect correct structure', () => {
+    const suite = createSuite('user', () => {
+      test('username', 'username is required', () => false);
+
+      group('some group', () => {
+        test('password', 'password is too short', () => false);
+      });
+    });
+
+    suite();
+  });
+});

483adbb0f2dc6530dae4d96159d316a54a486bc9
ealush <code@ealush.com>
2022-11-20 00:23:56 +0200

    about to do allIncomplete

--- packages/vest-5/src/group/group.ts
+++ packages/vest-5/src/group/group.ts
@@ -1,9 +1,10 @@
-import { currentGroup } from 'ctx';
+import { CurrentGroupContext } from 'ctx';
 import { isolate } from 'isolate';
+
 import { Isolate, IsolateTypes } from 'isolateTypes';
 
 export function group(name: string, callback: () => void): Isolate {
   return isolate(IsolateTypes.GROUP, () => {
-    currentGroup.run(name, callback);
+    CurrentGroupContext.run(name, callback);
   });
 }

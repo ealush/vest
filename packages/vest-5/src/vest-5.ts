e733737d2adfeee979eb850ca186fa9898e8e7dd
ealush <code@ealush.com>
2022-11-22 01:57:06 +0200

    add exclusion controls

--- packages/vest-5/src/vest-5.ts
+++ packages/vest-5/src/vest-5.ts
@@ -1,8 +1,21 @@
+import { skip, only } from 'exclusive';
 import { group } from 'group';
 import { enforce } from 'n4s';
+import { omitWhen } from 'omitWhen';
 import { optional } from 'optional';
+import { skipWhen } from 'skipWhen';
 import { test } from 'test';
 
 import { createSuite } from 'createSuite';
 
-export { createSuite, test, group, optional, enforce };
+export {
+  createSuite,
+  test,
+  group,
+  optional,
+  enforce,
+  skip,
+  skipWhen,
+  omitWhen,
+  only,
+};

83c153f4d5dde3299ad0f27da8028f8177330bf3
ealush <code@ealush.com>
2022-11-22 21:08:30 +0200

    vest@5: Add suite selectors

--- packages/vest-5/src/suiteResult/suiteResult.ts
+++ packages/vest-5/src/suiteResult/suiteResult.ts
@@ -1,8 +1,12 @@
+import { suiteSelectors, SuiteSelectors } from 'suiteSelectors';
+import { assign } from 'vest-utils';
+
 import { SuiteSummary } from 'SuiteResultTypes';
 import { produceSuiteSummary } from 'produceSuiteSummary';
 
 export function suiteResult(): SuiteResult {
-  return produceSuiteSummary();
+  const summary = produceSuiteSummary();
+  return assign(summary, suiteSelectors(summary));
 }
 
-export type SuiteResult = SuiteSummary;
+export type SuiteResult = SuiteSummary & SuiteSelectors;

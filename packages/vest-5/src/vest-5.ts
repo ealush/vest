b458851069bdc8cb53894ff66a92afea9d3f8051
ealush <code@ealush.com>
2022-11-23 01:29:34 +0200

    Include

--- packages/vest-5/src/vest-5.ts
+++ packages/vest-5/src/vest-5.ts
@@ -1,13 +1,14 @@
-import { enforce } from 'n4s';
-
-import { createSuite } from 'createSuite';
 import { skip, only } from 'exclusive';
 import { group } from 'group';
+import { include } from 'include';
+import { enforce } from 'n4s';
 import { omitWhen } from 'omitWhen';
 import { optional } from 'optional';
 import { skipWhen } from 'skipWhen';
 import { test } from 'test';
 import { warn } from 'warn';
+
+import { createSuite } from 'createSuite';
 
 export {
   createSuite,
@@ -20,4 +21,5 @@ export {
   omitWhen,
   only,
   warn,
+  include,
 };

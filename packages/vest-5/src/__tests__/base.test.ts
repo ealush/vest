c382195803941e47270146d574127913e5200695
ealush <code@ealush.com>
2022-11-26 00:58:11 +0200

    vest@5: First reconciler iteration works

--- packages/vest-5/src/__tests__/base.test.ts
+++ packages/vest-5/src/__tests__/base.test.ts
@@ -1,14 +1,21 @@
-import { createSuite, test as vestTest, include, only } from 'vest-5';
+import { createSuite, test as vestTest, only } from 'vest-5';
 
 describe('base scenario', () => {
   it('Should reflect correct structure', () => {
+    let counter = 0;
     const suite = createSuite('user', () => {
-      only('username');
+      if (counter) {
+        only('xxxxxxxxxx');
+      }
 
-      include('password').when('username');
+      // include('password').when('username');
 
-      vestTest('username', () => false);
+      vestTest('username', () => {
+        console.log('running');
+        return false;
+      });
       vestTest('password', 'password is too short', () => false);
+      counter++;
     });
 
     // console.log(suite());

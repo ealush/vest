545ca0b52ddc13ce4d32f09d963c28851115c1db
ealush <code@ealush.com>
2022-11-25 23:36:52 +0200

    Support Test Key

--- packages/vest-5/src/test/test.ts
+++ packages/vest-5/src/test/test.ts
@@ -1,23 +1,39 @@
 import { IsolateTypes } from 'IsolateTypes';
-import { useGroupName } from 'SuiteContext';
-import { TestFn } from 'TestTypes';
 import { VestTest } from 'VestTest';
 import { isolate } from 'isolate';
+import { isFunction } from 'vest-utils';
+
+import { useGroupName } from 'SuiteContext';
+import { TestFn } from 'TestTypes';
 import { attemptRunTestObjectByTier } from 'runTest';
 
 function vestTest(fieldName: string, message: string, cb: TestFn): VestTest;
 function vestTest(fieldName: string, cb: TestFn): VestTest;
 function vestTest(
   fieldName: string,
-  ...args: [message: string, cb: TestFn] | [cb: TestFn]
+  message: string,
+  cb: TestFn,
+  key: string
+): VestTest;
+function vestTest(fieldName: string, cb: TestFn, key: string): VestTest;
+function vestTest(
+  fieldName: string,
+  ...args:
+    | [message: string, cb: TestFn]
+    | [cb: TestFn]
+    | [message: string, cb: TestFn, key: string]
+    | [cb: TestFn, key: string]
 ): VestTest {
-  const [cb, message] = args.reverse() as [TestFn, string | undefined];
+  const [message, testFn, key] = (
+    isFunction(args[1]) ? args : [undefined, ...args]
+  ) as [string | undefined, TestFn, string | undefined];
 
   const groupName = useGroupName();
 
-  const testObject = new VestTest(fieldName, cb, {
+  const testObject = new VestTest(fieldName, testFn, {
     message,
     groupName,
+    key,
   });
 
   isolate(

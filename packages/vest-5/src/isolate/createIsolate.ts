5d640299db6a20dc630b6d894b0896573f85faa5
ealush <code@ealush.com>
2022-11-26 02:22:09 +0200

    vest@5: Keep test key within isolate

--- packages/vest-5/src/isolate/createIsolate.ts
+++ packages/vest-5/src/isolate/createIsolate.ts
@@ -2,13 +2,14 @@ import { Isolate, IsolateTypes } from 'IsolateTypes';
 
 export function createIsolate(
   type: IsolateTypes = IsolateTypes.DEFAULT,
-  parent = null,
+  parent: null | Isolate = null,
   data?: any
 ): Isolate {
   const isolate = {
     children: [],
     cursor: 0,
     data,
+    keys: {},
     parent,
     type,
   };

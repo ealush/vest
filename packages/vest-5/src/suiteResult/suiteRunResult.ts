a3969244e2665c7cf20654c4c1c5441b3a856552
ealush <code@ealush.com>
2022-12-12 02:49:55 +0200

    Done tests pass

--- packages/vest-5/src/suiteResult/suiteRunResult.ts
+++ packages/vest-5/src/suiteResult/suiteRunResult.ts
@@ -1,13 +1,10 @@
-import { assign, isFunction, numberEquals } from 'vest-utils';
+import { assign } from 'vest-utils';
 
-import {
-  DoneCallback,
-  persist,
-  useDoneCallbacks,
-  useFieldCallbacks,
-} from 'PersistedContext';
+import { persist } from 'PersistedContext';
 import { Done, SuiteResult, SuiteRunResult } from 'SuiteResultTypes';
-import { TestWalker } from 'SuiteWalker';
+import { deferDoneCallback } from 'deferDoneCallback';
+import { shouldRunDoneCallback } from 'shouldRunDoneCallback';
+import { shouldSkipDoneRegistration } from 'shouldSkipDoneRegistration';
 import { suiteResult } from 'suiteResult';
 
 export function suiteRunResult(): SuiteRunResult {
@@ -43,50 +40,3 @@ const done: Done = function done(...args): SuiteRunResult {
 
   return output;
 };
-
-/**
- * DONE is here and not in its own module to prevent circular dependency issues.
- */
-
-function shouldSkipDoneRegistration(
-  callback: (res: SuiteResult) => void,
-
-  fieldName: string | undefined,
-  output: SuiteRunResult
-): boolean {
-  // If we do not have any test runs for the current field
-  return !!(
-    !isFunction(callback) ||
-    (fieldName && numberEquals(output.tests[fieldName]?.testCount, 0))
-  );
-}
-
-// TODO: This check also happens when tests are finished. Need to combine them.
-function shouldRunDoneCallback(fieldName?: string): boolean {
-  // is suite finished || field name exists, and tests are finished;
-
-  return !!(
-    !TestWalker.hasRemainingTests() ||
-    (fieldName && !TestWalker.hasRemainingTests(fieldName))
-  );
-}
-
-function deferDoneCallback(
-  doneCallback: DoneCallback,
-  fieldName?: string
-): void {
-  const [, setFieldCallbacks] = useFieldCallbacks();
-  const [, setDoneCallbacks] = useDoneCallbacks();
-
-  if (fieldName) {
-    setFieldCallbacks(fieldCallbacks =>
-      assign(fieldCallbacks, {
-        [fieldName]: (fieldCallbacks[fieldName] || []).concat(doneCallback),
-      })
-    );
-
-    return;
-  }
-
-  setDoneCallbacks(doneCallbacks => doneCallbacks.concat(doneCallback));
-}

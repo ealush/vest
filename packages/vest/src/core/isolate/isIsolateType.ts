2116a43b6229aebd71ec7599310ecb2d74ea90f2
ealush <code@ealush.com>
2023-01-14 23:58:09 +0200

    Improve reconciler correctness by canceling overridden callbackes

--- packages/vest/src/core/isolate/isIsolateType.ts
+++ packages/vest/src/core/isolate/isIsolateType.ts
@@ -1,9 +1,9 @@
-import { Isolate, IsolateTypes } from 'IsolateTypes';
+import { Isolate, IsolateTest, IsolateTypes } from 'IsolateTypes';
 
 export function isIsolateType(isolate: Isolate, type: IsolateTypes): boolean {
   return isolate.type === type;
 }
 
-export function isTestIsolate(isolate: Isolate): boolean {
+export function isTestIsolate(isolate: Isolate): isolate is IsolateTest {
   return isIsolateType(isolate, IsolateTypes.TEST);
 }

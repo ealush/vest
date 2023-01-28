901ec541cc30b1f4e1d787b6d6e4f6ede1c5a2df
ealush <code@ealush.com>
2023-01-28 12:39:45 -0600

    generic creation of isolate

--- packages/vest/src/core/isolate/reconciler/VestReconciler/testHasKey.ts
+++
@@ -1,7 +0,0 @@
-import { isNotNullish } from 'vest-utils';
-
-import { VestTest } from 'VestTest';
-
-export function testHasKey(testObject: VestTest): boolean {
-  return isNotNullish(testObject.key);
-}

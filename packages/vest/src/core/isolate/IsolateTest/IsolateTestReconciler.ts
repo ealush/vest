a3a00e46bdd2983198605aac132f5aab8215959e
ealush <code@ealush.com>
2023-02-19 00:54:11 +0200

    vest@5: Merge repeating PickNode/VerifyTestRun logic

--- packages/vest/src/core/isolate/IsolateTest/IsolateTestReconciler.ts
+++ packages/vest/src/core/isolate/IsolateTest/IsolateTestReconciler.ts
@@ -1,15 +1,11 @@
-import { isOptionalFiedApplied } from 'optional';
 import { deferThrow, isNullish } from 'vest-utils';
 
 import { IsolateTest } from 'IsolateTest';
 import { Reconciler } from 'Reconciler';
 import cancelOverriddenPendingTest from 'cancelOverriddenPendingTest';
-import { isExcluded } from 'exclusive';
 import { isSameProfileTest } from 'isSameProfileTest';
 import { Isolate } from 'isolate';
-import { shouldSkipBasedOnMode } from 'mode';
-import { withinActiveOmitWhen } from 'omitWhen';
-import { isExcludedIndividually } from 'skipWhen';
+import { verifyTestRun } from 'verifyTestRun';
 
 export class IsolateTestReconciler extends Reconciler {
   static reconciler(
@@ -71,19 +67,7 @@ export class IsolateTestReconciler extends Reconciler {
   static pickNode(historyNode: IsolateTest, currentNode: IsolateTest): Isolate {
     const collisionResult = this.handleCollision(currentNode, historyNode);
 
-    if (shouldSkipBasedOnMode(currentNode)) {
-      return skipTestAndReturn(currentNode);
-    }
-
-    if (shouldOmit(currentNode)) {
-      return omitTestAndReturn(currentNode);
-    }
-
-    if (isExcluded(currentNode)) {
-      return forceSkipIfInSkipWhen(collisionResult);
-    }
-
-    return currentNode;
+    return verifyTestRun(currentNode, collisionResult);
   }
 
   static handleNoHistoryNode(testNode: IsolateTest): IsolateTest {
@@ -103,29 +87,6 @@ function cancelOverriddenPendingTestOnTestReRun(
   if (nextNode === currentNode && IsolateTest.is(currentNode)) {
     cancelOverriddenPendingTest(prevTestObject, currentNode);
   }
-}
-
-function shouldOmit(testObject: IsolateTest): boolean {
-  return withinActiveOmitWhen() || isOptionalFiedApplied(testObject.fieldName);
-}
-
-function skipTestAndReturn(testNode: IsolateTest): IsolateTest {
-  testNode.skip();
-  return testNode;
-}
-
-function omitTestAndReturn(testNode: IsolateTest): IsolateTest {
-  testNode.omit();
-  return testNode;
-}
-
-function forceSkipIfInSkipWhen(testNode: IsolateTest): IsolateTest {
-  // We're forcing skipping the pending test
-  // if we're directly within a skipWhen block
-  // This mostly means that we're probably giving
-  // up on this async test intentionally.
-  testNode.skip(isExcludedIndividually());
-  return testNode;
 }
 
 function throwTestOrderError(

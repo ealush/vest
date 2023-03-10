813ac177713c8e1d8c320afe9afbc994e2cee22b
ealush <code@ealush.com>
2023-03-10 02:38:03 +0200

    vx: better directive support

---
+++ vx/eslint-plugin-vest-internal/lib/rules/common/matchers.js
@@ -0,0 +1,11 @@
+const USE_MATCHER = /use.*/;
+const FUNC_DEC = 'FunctionDeclaration';
+const VAR_DEC = 'VariableDeclarator';
+const ARROW_EXP = 'ArrowFunctionExpression';
+
+module.exports = {
+  ARROW_EXP,
+  FUNC_DEC,
+  USE_MATCHER,
+  VAR_DEC,
+};
--- vx/eslint-plugin-vest-internal/lib/rules/use-use.js
+++ vx/eslint-plugin-vest-internal/lib/rules/use-use.js
@@ -1,5 +1,9 @@
 const path = require('path');
 
+const { isAllowed } = require('./common/directives');
+const { USE_MATCHER, VAR_DEC, FUNC_DEC } = require('./common/matchers');
+const { findAncestor, getLoc } = require('./common/selectors');
+
 const RULE_NAME = path.basename(__filename, path.extname(__filename));
 
 module.exports = {
@@ -36,37 +40,12 @@ module.exports = {
   },
 };
 
-function isSilenced(context, node) {
-  const nodeToCheckForComments =
-    node.loc.start.column === 0 ? node : node.parent;
-
-  return context.getCommentsBefore(nodeToCheckForComments).some(comment => {
-    if (!comment.value) {
-      return false;
-    }
-
-    const [directive, rules] = comment.value.trim().split(' ');
-
-    if (directive !== ALLOW_DIRECTIVE) {
-      return false;
-    }
-
-    const rulesArray = rules.split(',');
-
-    return rulesArray.includes(RULE_NAME);
-  });
-}
-
-function findAncestor(context, type) {
-  return context.getAncestors().find(isType(type));
-}
-
 function matcher(type) {
   return `${type}${ID_NAME_MATCHER} ${CALL_EXPRESSION_MATCHER}`;
 }
 
 function report(context, node, id) {
-  if (isSilenced(context, node)) {
+  if (isAllowed(context, node, RULE_NAME)) {
     return;
   }
   return context.report({
@@ -91,36 +70,10 @@ function addUseToName(name) {
   return `use${name[0].toUpperCase()}${name.slice(1)}`;
 }
 
-function isType(type) {
-  return node => node.type === type;
-}
-
-function getLoc(node) {
-  const { start, end } = node.loc;
-
-  return {
-    loc: {
-      start: {
-        line: start.line,
-        column: start.column,
-      },
-      end: {
-        line: end.line,
-        column: end.column,
-      },
-    },
-  };
-}
-
 const suggest = "Rename function to start with 'use'";
 
 const message =
   "Function {{ identifier }} does not start with 'use' but contains a call to function that starts with 'use'";
 
-const USE_MATCHER = /use.*/;
 const CALL_EXPRESSION_MATCHER = `CallExpression:matches([callee.name=${USE_MATCHER}])`;
 const ID_NAME_MATCHER = `:not([id.name=${USE_MATCHER}])`;
-
-const FUNC_DEC = 'FunctionDeclaration';
-const VAR_DEC = 'VariableDeclarator';
-const ALLOW_DIRECTIVE = '@vx-allow';

79f78894958b14f2cf6b606601985b96e01f429e
ealush <code@ealush.com>
2023-04-15 21:52:13 -0600

    types(vest-utils): defaultTo - add nullish type support

--- packages/vest-utils/src/defaultTo.ts
+++ packages/vest-utils/src/defaultTo.ts
@@ -1,7 +1,7 @@
 import optionalFunctionValue from 'optionalFunctionValue';
 
 export default function defaultTo<T>(
-  value: T | ((...args: any[]) => T),
+  value: T | void | null | ((...args: any[]) => T | void | null),
   defaultValue: T | (() => T)
 ): T {
   return optionalFunctionValue(value) ?? optionalFunctionValue(defaultValue);

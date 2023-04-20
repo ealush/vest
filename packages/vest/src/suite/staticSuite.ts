a3cc477f8800e26676d34da33e50f1c6b25f0e43
ealush <code@ealush.com>
2023-04-19 21:28:27 -0700

    types(vest): static suite returns typed methods

--- packages/vest/src/suite/staticSuite.ts
+++ packages/vest/src/suite/staticSuite.ts
@@ -1,12 +1,24 @@
-import { CB } from 'vest-utils';
+import { CB, assign } from 'vest-utils';
 
 import { SuiteRunResult, TFieldName, TGroupName } from 'SuiteResultTypes';
 import { createSuite } from 'createSuite';
+import { TTypedMethods, getTypedMethods } from 'getTypedMethods';
 
 export function staticSuite<
   T extends CB,
   F extends TFieldName = string,
   G extends TGroupName = string
->(suiteCallback: T): (...args: Parameters<T>) => SuiteRunResult<F, G> {
-  return (...args) => createSuite<T, F, G>(suiteCallback)(...args);
+>(suiteCallback: T): StaticSuite<T, F, G> {
+  return assign(
+    (...args: Parameters<T>) => createSuite<T, F, G>(suiteCallback)(...args),
+    {
+      ...getTypedMethods<F, G>(),
+    }
+  );
 }
+
+type StaticSuite<
+  T extends CB,
+  F extends TFieldName = string,
+  G extends TGroupName = string
+> = ((...args: Parameters<T>) => SuiteRunResult<F, G>) & TTypedMethods<F, G>;

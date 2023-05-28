7866590f2a2d57f9608aa43e24abaec1ce09cdef
Evyatar <code@ealush.com>
2023-05-28 21:50:08 +0300

    next step - remove all unneeded exports

--- packages/vest-runtime/src/vest-runtime.ts
+++ packages/vest-runtime/src/vest-runtime.ts
@@ -1,4 +1,4 @@
 export { Isolate, IsolateKey } from 'Isolate';
-export { Reconciler, IRecociler, BaseReconciler } from 'Reconciler';
+export { Reconciler, IRecociler } from 'Reconciler';
 export * as walker from 'walker';
 export * as VestRuntime from 'VestRuntime';
--- packages/vest/src/hooks/optional/optional.ts
+++ packages/vest/src/hooks/optional/optional.ts
@@ -1,5 +1,5 @@
 import { VestRuntime } from 'vest-runtime';
-import { isArray, isStringValue, asArray } from 'vest-utils';
+import { isArray, isStringValue, asArray, invariant } from 'vest-utils';
 
 import type { IsolateSuite } from 'IsolateSuite';
 import { OptionalFieldTypes, OptionalsInput } from 'OptionalTypes';
@@ -9,7 +9,10 @@ import { TFieldName } from 'SuiteResultTypes';
 export function optional<F extends TFieldName>(
   optionals: OptionalsInput<F>
 ): void {
-  const suiteRoot = VestRuntime.useRuntimeRoot() as IsolateSuite;
+  const suiteRoot = VestRuntime.useAvailableRoot<IsolateSuite>();
+
+  // TODO: Add an error message here
+  invariant(suiteRoot);
 
   // There are two types of optional field declarations:
 

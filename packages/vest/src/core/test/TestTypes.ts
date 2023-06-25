e8e3e1c1348a6426583e13298797e8deae4eae1b
Dmitrii Maganov <vonagam@gmail.com>
2023-06-25 19:34:11 +0300

    types(vest): fix `AsyncTest`, async test does not expect any returns

--- packages/vest/src/core/test/TestTypes.ts
+++ packages/vest/src/core/test/TestTypes.ts
@@ -3,7 +3,7 @@ import { Maybe } from 'vest-utils';
 import { TFieldName } from 'SuiteResultTypes';
 
 export type TestFn = () => TestResult;
-export type AsyncTest = Promise<Maybe<string | false>>;
+export type AsyncTest = Promise<void>;
 export type TestResult = Maybe<AsyncTest | boolean>;
 
 export type WithFieldName<F extends TFieldName = TFieldName> = {

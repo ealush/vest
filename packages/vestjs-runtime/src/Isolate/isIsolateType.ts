c27f3beef741b820a28d1c74fcc4b5a405e8cd4b
Evyatar <code@ealush.com>
2023-07-07 23:59:34 +0300

    patch(vestjs-runtime): Reuse common reconciler capabilities

--- packages/vestjs-runtime/src/Isolate/isIsolateType.ts
+++ packages/vestjs-runtime/src/Isolate/isIsolateType.ts
@@ -3,3 +3,17 @@ import { Isolate } from 'Isolate';
 export function isIsolateType(node: Isolate, type: string): node is Isolate {
   return node.type === type;
 }
+
+export function isSameIsolateType<A extends Isolate, B extends Isolate>(
+  a: A,
+  b: B
+): boolean {
+  return a.type === b.type;
+}
+
+export function isSameIsolateIdentity<A extends Isolate, B extends Isolate>(
+  a: A,
+  b: B
+): boolean {
+  return Object.is(a, b) || (isSameIsolateType(a, b) && a.key === b.key);
+}
--- packages/vestjs-runtime/src/Reconciler.ts
+++ packages/vestjs-runtime/src/Reconciler.ts
@@ -4,19 +4,21 @@ import { type Isolate } from 'Isolate';
 import { IsolateInspector } from 'IsolateInspector';
 import { IsolateMutator } from 'IsolateMutator';
 import * as VestRuntime from 'VestRuntime';
+import { isSameIsolateType } from 'isIsolateType';
+// import { isSameIsolateType } from 'isIsolateType';
 
 // I would rather not use `any` here, but instead use `Isolate`.
 // The problem is that it breaks the actual implementation of `Isolate` in `IsolateTest`
 // As it is not properly extending `Isolate`.
 export interface IRecociler<I = any> {
-  (currentNode: I, historicNode: Nullable<Isolate>): I;
+  (currentNode: I, historyNode: Nullable<Isolate>): I;
 }
 
 export function BaseReconciler(
   currentNode: Isolate,
-  historicNode: Nullable<Isolate>
+  historyNode: Nullable<Isolate>
 ): Isolate {
-  if (isNullish(historicNode)) {
+  if (isNullish(historyNode)) {
     return currentNode;
   }
   return currentNode;
@@ -42,7 +44,8 @@ export class Reconciler {
       );
     }
 
-    const nextNode = reconciler(node, localHistoryNode);
+    // const nextNode = reconciler(node, localHistoryNode);
+    const nextNode = pickNextNode(reconciler, node, localHistoryNode);
 
     invariant(nextNode);
 
@@ -54,17 +57,17 @@ export class Reconciler {
   }
 
   static removeAllNextNodesInIsolate() {
-    const testIsolate = VestRuntime.useIsolate();
+    const currentNode = VestRuntime.useIsolate();
     const historyNode = VestRuntime.useHistoryNode();
 
-    if (!historyNode || !testIsolate) {
+    if (!historyNode || !currentNode) {
       // This is probably unreachable, but TS is not convinced.
       // Let's play it safe.
       /* istanbul ignore next */
       return;
     }
 
-    IsolateMutator.slice(historyNode, IsolateInspector.cursor(testIsolate));
+    IsolateMutator.slice(historyNode, IsolateInspector.cursor(currentNode));
   }
 
   static handleIsolateNodeWithKey(node: Isolate): Isolate {
@@ -104,4 +107,28 @@ function useRunAsNew<Callback extends CB = CB>(
 
   current.output = output;
   return output;
+}
+
+function pickNextNode(
+  reconciler: IRecociler,
+  currentNode: Isolate,
+  historyNode: Nullable<Isolate>
+): Isolate {
+  if (isNullish(historyNode)) {
+    return handleNoHistoryNode(currentNode);
+  }
+
+  if (!isSameIsolateType(currentNode, historyNode)) {
+    return currentNode;
+  }
+
+  return reconciler(currentNode, historyNode);
+}
+
+function handleNoHistoryNode<I extends Isolate>(newNode: I): I {
+  if (IsolateInspector.usesKey(newNode)) {
+    return Reconciler.handleIsolateNodeWithKey(newNode) as I;
+  }
+
+  return newNode;
 }

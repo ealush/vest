c523b67c85c4ad401e03b2ccd5ed688af6ac84d4
Evyatar <code@ealush.com>
2023-07-15 20:26:40 +0300

    patch(vest): cleanup unneeded test  properties

--- packages/vest/src/core/isolate/IsolateTest/IsolateTest.ts
+++ packages/vest/src/core/isolate/IsolateTest/IsolateTest.ts
@@ -1,4 +1,4 @@
-import { CB, Maybe, seq } from 'vest-utils';
+import { CB, Maybe } from 'vest-utils';
 import { IsolateKey, TIsolate, Isolate } from 'vestjs-runtime';
 
 import { TestStatus } from 'IsolateTestStateMachine';
@@ -6,7 +6,6 @@ import { TestSeverity } from 'Severity';
 import { TFieldName, TGroupName } from 'SuiteResultTypes';
 import { AsyncTest, TestFn } from 'TestTypes';
 import { VestIsolateType } from 'VestIsolateType';
-import { VestTestInspector } from 'VestTestInspector';
 
 export type TIsolateTest<
   F extends TFieldName = TFieldName,
@@ -37,22 +36,13 @@ export function IsolateTest<
     payload
   );
 
-  setIsolateTestValueOf(isolate);
-
   return isolate as TIsolateTest<F, G>;
 }
 
 export function IsolateTestBase() {
   return {
-    id: seq(),
     severity: TestSeverity.Error,
     status: TestStatus.UNTESTED,
-  };
-}
-
-export function setIsolateTestValueOf(test: TIsolateTest) {
-  test.valueOf = () => {
-    return !VestTestInspector.isFailing(test);
   };
 }
 
@@ -60,7 +50,6 @@ export type IsolateTestPayload<
   F extends TFieldName = TFieldName,
   G extends TGroupName = TGroupName
 > = CommonTestFields<F, G> & {
-  id: string;
   severity: TestSeverity;
   status: TestStatus;
   asyncTest?: AsyncTest;

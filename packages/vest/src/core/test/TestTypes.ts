7f336f0abe35b8ff78143fdc0b7a2416c92991eb
Dmitry Maganov <vonagam@gmail.com>
2023-12-16 21:36:58 +0300

    types(vest): remove `void` from `Maybe<T>` and update `SuiteSelectors` (#1074)

--- packages/vest/src/core/test/TestTypes.ts
+++ packages/vest/src/core/test/TestTypes.ts
@@ -6,7 +6,7 @@ type TestFnPayload = { signal: AbortSignal };
 
 export type TestFn = (payload: TestFnPayload) => TestResult;
 export type AsyncTest = Promise<void>;
-export type TestResult = Maybe<AsyncTest | boolean>;
+export type TestResult = Maybe<AsyncTest | boolean> | void;
 
 export type WithFieldName<F extends TFieldName = TFieldName> = {
   fieldName: F;

d82873d7b866abff9eb791d8b676cc3769f198c9
ealush <code@ealush.com>
2024-07-27 03:53:40 +0300

    Current state is not workingpp

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,9 +4,12 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    include: ['packages/*/**/__tests__/*.test.ts'],
   },
+  root: __dirname,
   plugins: [
     tsconfigPaths({
+      loose: true,
       projects: [
         'packages/vest',
         'packages/n4s',

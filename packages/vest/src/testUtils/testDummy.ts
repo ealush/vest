a3d5a13d01f0ed37f9e0cb85477c857a4f9081f2
Evyatar Alush <ealush@meta.com>
2025-02-02 10:31:12 +0200

    lint(vest): remove eslint disables

--- packages/vest/src/testUtils/testDummy.ts
+++ packages/vest/src/testUtils/testDummy.ts
@@ -1,138 +1,116 @@
 import { faker } from '@faker-js/faker';
 import { vi } from 'vitest';
+import wait from 'wait';
 
 import { test as vestTest, warn } from 'vest';
 
 /**
  * Generates dummy vest tests.
  */
-// eslint-disable-next-line max-lines-per-function
-const testDummy = () => {
-  const failing = (
-    name: string = faker.lorem.word(),
-    message: string = faker.lorem.words(),
-  ) => {
-    const to = vestTest(
-      name,
-      message,
-      vi.fn(() => {
-        throw new Error();
-      }),
-    );
+const createFailing = (
+  name: string = faker.lorem.word(),
+  message: string = faker.lorem.words(),
+) =>
+  vestTest(
+    name,
+    message,
+    vi.fn(() => {
+      throw new Error();
+    }),
+  );
 
-    return to;
-  };
+const createFailingWarning = (
+  name = faker.lorem.word(),
+  message = faker.lorem.words(),
+) =>
+  vestTest(
+    name,
+    message,
+    vi.fn(() => {
+      warn();
+      throw new Error();
+    }),
+  );
 
-  const failingWarning = (
-    name = faker.lorem.word(),
-    message = faker.lorem.words(),
-  ) => {
-    const to = vestTest(
-      name,
-      message,
-      vi.fn(() => {
-        warn();
-        throw new Error();
-      }),
-    );
+const createPassing = (
+  name = faker.lorem.word(),
+  message = faker.lorem.words(),
+) => vestTest(name, message, vi.fn());
 
-    return to;
-  };
+const createPassingWarning = (
+  name = faker.lorem.word(),
+  message = faker.lorem.words(),
+) =>
+  vestTest(
+    name,
+    message,
+    vi.fn(() => {
+      warn();
+    }),
+  );
 
-  const passing = (
-    name = faker.lorem.word(),
-    message = faker.lorem.words(),
-  ) => {
-    const to = vestTest(name, message, vi.fn());
+const createFailingAsync = (
+  name = faker.lorem.word(),
+  { message = faker.lorem.words(), time = 0 } = {},
+) =>
+  vestTest(
+    name,
+    message,
+    vi.fn(async () => {
+      await wait(time);
+      throw new Error();
+    }),
+  );
 
-    return to;
-  };
+const createFailingWarningAsync = (
+  name = faker.lorem.word(),
+  { message = faker.lorem.words(), time = 0 } = {},
+) =>
+  vestTest(
+    name,
+    message,
+    vi.fn(async () => {
+      warn();
+      await wait(time);
+      throw new Error();
+    }),
+  );
 
-  const passingWarning = (
-    name = faker.lorem.word(),
-    message = faker.lorem.words(),
-  ) => {
-    const to = vestTest(
-      name,
-      message,
-      vi.fn(() => {
-        warn();
-      }),
-    );
-    return to;
-  };
+const createPassingAsync = (
+  name = faker.lorem.word(),
+  { message = faker.lorem.words(), time = 0 } = {},
+) =>
+  vestTest(
+    name,
+    message,
+    vi.fn(async () => {
+      await wait(time);
+    }),
+  );
 
-  const failingAsync = (
-    name = faker.lorem.word(),
-    { message = faker.lorem.words(), time = 0 } = {},
-  ) =>
-    vestTest(
-      name,
-      message,
-      vi.fn(
-        () =>
-          new Promise((_, reject) => {
-            setTimeout(reject, time);
-          }),
-      ),
-    );
+const createPassingWarningAsync = (
+  name = faker.lorem.word(),
+  { message = faker.lorem.words(), time = 0 } = {},
+) =>
+  vestTest(
+    name,
+    message,
+    vi.fn(async () => {
+      warn();
+      await wait(time);
+    }),
+  );
 
-  const failingWarningAsync = (
-    name = faker.lorem.word(),
-    { message = faker.lorem.words(), time = 0 } = {},
-  ) =>
-    vestTest(
-      name,
-      message,
-      vi.fn(() => {
-        warn();
-        return new Promise((_, reject) => {
-          setTimeout(reject, time);
-        });
-      }),
-    );
-
-  const passingAsync = (
-    name = faker.lorem.word(),
-    { message = faker.lorem.words(), time = 0 } = {},
-  ) =>
-    vestTest(
-      name,
-      message,
-      vi.fn(
-        () =>
-          new Promise(resolve => {
-            setTimeout(resolve, time);
-          }),
-      ),
-    );
-
-  const passingWarningAsync = (
-    name = faker.lorem.word(),
-    { message = faker.lorem.words(), time = 0 } = {},
-  ) =>
-    vestTest(
-      name,
-      message,
-      vi.fn(() => {
-        warn();
-        return new Promise(resolve => {
-          setTimeout(resolve, time);
-        });
-      }),
-    );
-
-  return {
-    failing,
-    failingAsync,
-    failingWarning,
-    failingWarningAsync,
-    passing,
-    passingAsync,
-    passingWarning,
-    passingWarningAsync,
-  };
-};
+const testDummy = () => ({
+  failing: createFailing,
+  failingAsync: createFailingAsync,
+  failingWarning: createFailingWarning,
+  failingWarningAsync: createFailingWarningAsync,
+  passing: createPassing,
+  passingAsync: createPassingAsync,
+  passingWarning: createPassingWarning,
+  passingWarningAsync: createPassingWarningAsync,
+});
 
 export const dummyTest = testDummy();
 
